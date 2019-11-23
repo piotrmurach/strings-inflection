d7fbe22cde90ec9ac9977f41c3b295e36e25bbff
Piotr Murach <pmurach@gmail.com>
2019-11-23 20:50:51 +0000

    Change fixture to name the type of inflections

--- spec/unit/pluralize_spec.rb
+++ spec/unit/pluralize_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Strings::Inflect, "#pluralize" do
-  inflections_example("inflections") do |singular, plural|
+  inflections_example("nouns") do |singular, plural|
     it "pluralizes #{singular} -> #{plural}" do
       expect(Strings::Inflect.pluralize(singular)).to eq(plural)
     end
--- spec/unit/singularize_spec.rb
+++ spec/unit/singularize_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Strings::Inflect, "#singularize" do
-  inflections_example("inflections") do |singular, plural|
+  inflections_example("nouns") do |singular, plural|
     it "singularizes #{plural} -> #{singular}" do
       expect(Strings::Inflect.singularize(plural)).to eq(singular)
     end
