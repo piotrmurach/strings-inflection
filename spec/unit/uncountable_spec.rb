955812ae9862987778d0099a7d07b304d1042b08
Piotr Murach <pmurach@gmail.com>
2019-11-23 20:27:50 +0000

    Add tests for uncountable call

---
+++ spec/unit/uncountable_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Strings::Inflect, "#uncountable?" do
+  it "checks if noun is uncountable" do
+    expect(Strings::Inflect.uncountable?("sheep")).to eq(true)
+  end
+
+  it "checks if noun is countable" do
+    expect(Strings::Inflect.uncountable?("error")).to eq(false)
+  end
+end
