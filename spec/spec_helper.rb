4f04948b44da85d3d3d1d10fa7fccc68ca70f767
Piotr Murach <pmurach@gmail.com>
2019-11-28 21:11:27 +0000

    Change to expose helpers inside examples

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,6 +45,7 @@ end
 
 RSpec.configure do |config|
   config.extend(Helpers)
+  config.include(Helpers)
 
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
