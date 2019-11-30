26409135e1886a6e0ec8b99d4a873ef6b0cacd13
Piotr Murach <pmurach@gmail.com>
2019-11-30 15:12:57 +0000

    Change parser to stop passing internal state

--- lib/strings/inflect/parser.rb
+++ lib/strings/inflect/parser.rb
@@ -11,73 +11,81 @@ module Strings
       #
       # @api private
       def self.parse(str, count)
-        scanner = StringScanner.new(str)
-        value = []
-
-        while !scanner.eos?
-          parse_noun(scanner, count, value) ||
-          parse_verb(scanner, count, value) ||
-          parse_count(scanner, count, value) ||
-          parse_char(scanner, count, value)
-        end
-
-        value.join
+        parser = new(str, count)
+        parser.parse
       end
 
+      def initialize(str, count)
+        @scanner = StringScanner.new(str)
+        @count = count
+        @value = []
+      end
+
+      def parse
+        while !@scanner.eos?
+          parse_noun || parse_verb ||
+          parse_count || parse_char
+        end
+
+        @value.join
+      end
+
+      private
+
       # @api private
-      def self.parse_noun(scanner, count, value)
-        if scanner.scan(/\{\{N([^\}]*?):([^\}]+?)\}\}/)
-          option = scanner[1].to_s.tr(" ", "").downcase
+      def parse_noun
+        if @scanner.scan(/\{\{N([^\}]*?):([^\}]+?)\}\}/)
+          option = @scanner[1].to_s.tr(" ", "").downcase
           if option =~ /[^sp]/i
             raise "Unknown option '#{option}' in {{N:...}} tag"
           end
           inflection = if option.empty?
-                         count == 1 ? :singular : :plural
+                         @count == 1 ? :singular : :plural
                        else
                          option == "s" ? :singular : :plural
                        end
-          noun = Noun[scanner[2].to_s.tr(" ", "")]
-          value << noun.public_send(inflection)
+          noun = Noun[@scanner[2].to_s.tr(" ", "")]
+          @value << noun.public_send(inflection)
         end
       end
 
       # @api private
-      def self.parse_verb(scanner, count, value)
-        if scanner.scan(/\{\{V([^\}]*?):([^\}]+?)\}\}/)
-          option = scanner[1].to_s.tr(" ", "").downcase
+      def parse_verb
+        if @scanner.scan(/\{\{V([^\}]*?):([^\}]+?)\}\}/)
+          option = @scanner[1].to_s.tr(" ", "").downcase
           if !option.empty?
             raise "Unknown option '#{option}' in {{V:...}} tag"
           end
-          inflection = count == 1 ? :singular : :plural
-          verb = Verb[scanner[2].to_s.tr(" ", "")]
-          value << verb.public_send(inflection)
+          inflection = @count == 1 ? :singular : :plural
+          verb = Verb[@scanner[2].to_s.tr(" ", "")]
+          @value << verb.public_send(inflection)
         end
       end
 
       # @api private
-      def self.parse_count(scanner, count, value)
-        if scanner.scan(/\{\{#([^\}]*?):([^\}]+?)\}\}/)
-          option = scanner[1].to_s.tr(" ", "").downcase
+      def parse_count
+        if @scanner.scan(/\{\{#([^\}]*?):([^\}]+?)\}\}/)
+          option = @scanner[1].to_s.tr(" ", "").downcase
           if option =~ /[^f]/
             raise "Unknown option '#{option}' in {{#:...}} tag"
           end
           amount = case option
                    when "f"
-                     fuzzy_count(count)
+                     fuzzy_count(@count)
                    else
-                     count
+                     @count
                    end
-          value << amount
+          @value << amount
         end
       end
 
       # @api private
-      def self.parse_char(scanner, count, value)
-        value << scanner.getch
+      def parse_char
+        @value << @scanner.getch
       end
 
       # @api private
-      def self.fuzzy_count(count)
+      def fuzzy_count(count)
         if count >= 10 then "many"
         elsif count >= 6 then "several"
         elsif count >= 3 then "a few"
