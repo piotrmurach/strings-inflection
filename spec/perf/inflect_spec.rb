00f69b394cc2b65fe44ef040df064c5b45d3b975
Piotr Murach <pmurach@gmail.com>
2019-11-30 16:38:03 +0000

    Add perf spec for comparison with active support

---
+++ spec/perf/inflect_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "rspec-benchmark"
+require "active_support"
+
+RSpec.describe Strings::Inflect do
+  include RSpec::Benchmark::Matchers
+
+  it "pluralizes nouns as fast as ActiveSupport" do
+    expect {
+      Strings::Inflect.pluralize("error")
+    }.to perform_slower_than {
+      ActiveSupport::Inflector.pluralize("error")
+    }.at_most(1.3).times
+  end
+
+  it "allocates" do
+    expect {
+      Strings::Inflect.pluralize("error")
+    }.to perform_allocation(40)
+  end
+end
